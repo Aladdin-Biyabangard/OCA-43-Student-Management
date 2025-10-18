a6e26d2248e8855601f809d49862bda83b91e0c6
subhanism <subhanism656@gmail.com>
2025-10-18 15:51:14 +0400

    addStudentToCourse method added

--- src/service/CourseService.java
+++ src/service/CourseService.java
@@ -2,12 +2,17 @@ package service;
 
 import entity.CourseEntity;
 import repository.CourseRepository;
+import repository.StudentRepository;
+
+import java.util.Map;
 
 public class CourseService {
     private CourseRepository courseRepository;
+    private StudentRepository studentRepository;
 
-    public CourseService(CourseRepository courseRepository) {
+    public CourseService(CourseRepository courseRepository, StudentRepository studentRepository) {
         this.courseRepository = courseRepository;
+        this.studentRepository = studentRepository;
     }
 
     public void addCourse(CourseEntity course) {
@@ -26,5 +31,14 @@ public class CourseService {
             return true;
         }
         return false;
+    }
+
+    public void addStudentToCourse(String courseId, String studentId) {
+        Map<String, CourseEntity> courses = courseRepository.getCourses();
+
+        CourseEntity courseEntity = courses.get(courseId);
+        courseEntity.getStudentIds().add(studentId);
+        courses.put(courseId,courseEntity);
+
     }
 }
