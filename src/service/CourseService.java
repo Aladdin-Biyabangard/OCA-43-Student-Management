825e2cc3d1e18e4e220d0bf579f5d0d10bb327a7
Bahar Jafarli <baharjafarli27@gmail.com>
2025-10-23 18:57:47 +0400

    enrollToCourse

--- src/service/CourseService.java
+++ src/service/CourseService.java
@@ -1,14 +1,33 @@
 package service;
 
 import entity.CourseEntity;
+import repository.CourseRepository;
 
 import java.util.List;
 import java.util.Map;
 
 public class CourseService {
 
-    public CourseEntity getCourse(String courseId){
-        Map<String, CourseEntity> courses =  courseRepository.getCourses();
+    private CourseRepository courseRepository;
 
+    public CourseService(CourseRepository courseRepository){
+        this.courseRepository = courseRepository;
+    }
+
+    public void addCourse(CourseEntity course){
+        courseRepository.getCourses().put(course.getId(), course);
+    }
+
+    public void updateCourse(CourseEntity course){
+        courseRepository.getCourses().put(course.getId(), course);
+    }
+    
+    public void removeCourse(String courseId){
+        courseRepository.getCourses().remove(courseId);
+    }
+
+
+    public List<CourseEntity> getCourses(){
+        return List.copyOf(courseRepository.getCourses().values());
     }
 }
--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -2,24 +2,45 @@ package service;
 
 import entity.CourseEntity;
 import entity.StudentEntity;
+import repository.CourseRepository;
 import repository.StudentRepository;
 
 import java.util.List;
 import java.util.Map;
 
+
 public class StudentService {
     private StudentRepository studentRepository;
-    private CourseService courseService;
-    private
+    private CourseRepository courseRepository;
 
-    public StudentService(StudentRepository studentRepository){
+    public StudentService(StudentRepository studentRepository, CourseRepository courseRepository){
         this.studentRepository = studentRepository;
+        this.courseRepository = courseRepository;
     }
 
-    public StudentService(CourseService courseService){
-        this.courseService = courseService;
+
+
+    public void addStudent(StudentEntity student){
+        studentRepository.getStudents().put(student.getId(),student);
     }
-    public void enrollStudent(){
-        CourseEntity course = courseService.getCourse(String.valueOf(1));
+
+    public void removeStudent(String studentId){
+        studentRepository.getStudents().remove(studentId);
     }
+
+    public void updateStudent(StudentEntity student){
+        studentRepository.getStudents().put(student.getId(), student);
+    }
+
+    public List<StudentEntity> getStudents(){
+        return List.copyOf(studentRepository.getStudents().values());
+    }
+
+    public void enrollToCourse(String studentId, String courseId){
+        Map<String, CourseEntity> courses = courseRepository.getCourses();
+        CourseEntity courseEntity = courses.get(courseId);
+        courseEntity.getStudentIds().add(studentId);
+        courses.put(courseId, courseEntity);
+    }
+
 }
